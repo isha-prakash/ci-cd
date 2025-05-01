d6a0836af5325e8256b34227dd04bfe5d43b73ba
isha-prakash <ishaprakash616@gmail.com>
2025-05-01 20:33:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>My react app</h1>
+      <h2>Implemented with ci/cd pipeline</h2>
+    </div>
+    
+  )
+}
+
+export default App
